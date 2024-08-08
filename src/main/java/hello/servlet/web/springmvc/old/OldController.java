69a6e480f82a7c28a54890f3b25f4b46b2547227
otteru <otteru@naver.com>
2024-08-08 13:55:28 +0900

    핸들러매핑과 핸들러 어댑터 중간

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller")//스프링 빈의 이름 지정/ URL 이름으로 맞춰준 것
+//스프링 빈의 이름의 핸들러 매칭함
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
